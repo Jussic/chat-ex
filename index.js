db6b4a7a131e7f0ac68d65a432beb3bc2352346f
J. Clarke <justineclarkes@gmail.com>
2016-01-03 11:20:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app = require('express')(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
+    ent = require('ent'), // Blocks HTML characters (security equivalent to htmlentities in PHP)
+    fs = require('fs');
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+// Loading the page index.html
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
 });
 
+
 // This goes through everyone connected on the server and 
-// sends out msg to them 
+// sends out msg to server 
 
 io.emit('some event', { for: 'everyone' })
 
@@ -18,9 +22,23 @@ socket.on('chat message', function(msg) {
 io.emit('chat message', msg);
 console.log('user disconnected');
 }); 
+
+io.sockets.on('connection', function (socket, username) {
+    // When the username is received it’s stored as a session variable and informs the other people
+    socket.on('new_client', function(username) {
+        username = ent.encode(username);
+        socket.username = username;
+        socket.broadcast.emit('new_client', username);
+    });
+
+    // When a message is received, get client’s username and sent to the others connected
+    socket.on('message', function (message) {
+        message = ent.encode(message);
+        socket.broadcast.emit('message', {username: socket.username, message: message});
+    }); 
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+server.listen(8080);
+console.log('listening on *:8080');
+
 
